2223fc253781f362dcc3eb6fb5a5d256328d7374
Nils Jansen <nj_jansen@t-online.de>
2017-02-16 11:43:13 +0100

    Copied clean version of Alecs, overwrote my copy with it, and changed paths

--- exercise1_Nils.R
+++ exercise1_Nils.R
@@ -1,102 +1,95 @@
-# FOR THE LOVE OF GOD, DO NOT USE THE RUN BUTTON. JUST PRESS CTRL+R
-library(class)
+source('A:/Machine_Learning/Basefolder/loadImage.R') 
 
-source ('A:/Machine_Learning/Basefolder/loadImage.R')
+#####################################
+# Exercise 1.4
+# change the top 4 variables to do the different tasks
+#####################################
 
-q=0
+test_split=0.5  #how large should the training set be 0.9=90/10 training/testing
+runs=1          #how many times to run this loop with different random seeds
+k_runs=1        #number of different k values to run
+k_start=10      #starting k value. will just be the k used if k_runs=1
+
 x=loadSinglePersonsData(300,4,2,"A:/Machine_Learning/2017/group") 
-avg_data<-array(0,dim=c(10,2))
-sd<-array(0,dim=c(10,2))
-for(q in 1:10)
+
+data_out<-array(0,dim=c(runs,k_runs,2))
+#sd<-array(0,dim=c(10,2))
+
+for(q in 1:runs)
 {
-set.seed(990+q)
-dataset_shuffle <- x[sample(nrow(x)),]
-
-
-# Exercise 1.4.4
-# smoothImage <- function(greyImg){
-#   kernel = makeBrush(3, shape = "Gaussian", step = TRUE, sigma = 0.55)
-#   print(kernel)
-#   smoothed= filter2(greyImg, kernal)
-#   return(smoothed)
-#   }
-
-dataset_train<- array(, dim=c(2000,3365))
-for(i in 1:2000)
-{
-  #kNN training set
-  dataset_train[i,]<-dataset_shuffle[i,]
-}
-
-dataset_test<- array(, dim=c(2000,3365))
-for(i in 1:2000)
-{
-  #kNN testing set
-  dataset_test[i,]<-dataset_shuffle[i+2000,]
-}
-
-cl<- array(, dim=c(1,2000))
-for(i in 1:2000)
-{
-  cl[i]=dataset_train[i,1]
-}
-
-dl<- array(, dim=c(1,2000))
-for(i in 1:2000)
-{
-  dl[i]=dataset_test[i,1]
-}
-
-
-p=80
-stats_out<-array(,dim=c(p,2))
-#K FOR KNN TEST
-k=10
-
-for(k in 10:p)
-{
-ptm<-proc.time()
-test_pred<-knn(dataset_train, dataset_test,cl,k)
-
-#CrossTable(dl,test_pred,prop.chisq = FALSE)
-correct=0
-incorrect=0
-for(i in 1:2000)
-{
-  if(test_pred[i]==dl[i])
+  set.seed(990+q)
+  dataset_shuffle <- x[sample(nrow(x)),]
+  
+  
+  #create the training set
+  dataset_train<- array(, dim=c((4000*test_split),3365))
+  for(i in 1:dim(dataset_train)[1])
   {
-    correct=correct + 1
+    #kNN training set
+    dataset_train[i,]<-dataset_shuffle[i,]
   }
-  else
+  
+  #create the testing set
+  dataset_test<- array(, dim=c(4000-dim(dataset_train)[1],3365))
+  for(i in 1:dim(dataset_test)[1])
   {
-    incorrect = incorrect + 1
+    #kNN testing set
+    dataset_test[i,]<-dataset_shuffle[i+(4000*test_split),]
   }
+  
+  #training set classification vector
+  train_class<- array(, dim=c(1,dim(dataset_train)[1]))
+  for(i in 1:dim(dataset_train)[1])
+  {
+    train_class[i]=dataset_train[i,1]
+  }
+  
+  #testing set classification vector
+  test_class<- array(, dim=c(1,dim(dataset_test)[1]))
+  for(i in 1:dim(dataset_test)[1])
+  {
+    test_class[i]=dataset_test[i,1]
+  }
+  
+  for(k in k_start:(k_start+k_runs-1))
+  {
+      
+    #start timing
+    ptm<-proc.time()
+    
+    #run knn test
+    test_pred<-knn(dataset_train, dataset_test,train_class,k)
+
+    
+    #check accuracy
+    correct=0
+    incorrect=0
+    for(i in 1:dim(dataset_test)[1])
+    {
+      if(test_pred[i]==test_class[i])
+      {
+        correct=correct + 1
+      }
+      else
+      {
+        incorrect = incorrect + 1
+      }
+    }
+    accuracy=correct/dim(dataset_test)[1]*100
+    #stop timing
+    ptm<-proc.time()-ptm
+    
+    data_out[q,(k-k_start+1),1]=accuracy
+    data_out[q,(k-k_start+1),2]=ptm[3]
+    
+  }
+  
 }
-ptm<-proc.time()-ptm
-stats_out[k,2]=correct/2000*100
-stats_out[k,1]=ptm[3]
-#avg_data[q,2]=correct/400*100
-#avg_data[q,1]=ptm[3]
-}
 
-}
+#use View(data_out[q,,]) to view specific run stats. column 1 is accuracy. replace q with a run number
+#use View(data_out[,k,]) to view specific run stats. column 1 is accuracy. replace k with a desired k to view
+#OR uncomment the one below to do all
 
-sd2=array(0,dim=c(1,2))
-time_avg=0
-acc_avg=0
-for(w in 1:10)
-{
-  time_avg=time_avg+(avg_data[w,1]/10)
-  acc_avg=acc_avg+(avg_data[w,2]/10)
-}
-for(w in 1:10)
-{
-  sd2[1]=sd2[1]+(avg_data[w,1]-time_avg)*(avg_data[w,1]-time_avg)
-  sd2[2]=sd2[2]+(avg_data[w,1]-acc_avg)*(avg_data[w,1]-acc_avg)
-}
-sd2[1]=sqrt(sd2[1]/10)
-sd2[2]=sqrt(sd2[2]/10)
+#for(i in 1:q){View(data_out[i,,])}
 
-
-
-
+#for(i in 1:k_runs){View(data_out[,i,])}
