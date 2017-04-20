94eb9827e4008b62ff98d5fad79af9a9b0de500e
nilsjansen <nj_jansen@t-online.de>
2017-04-20 09:40:28 +0200

    Added folder and file for ex4

--- Exercise3/exercise_3_2_nils.R
+++ Exercise3/exercise_3_2_nils.R
@@ -18,28 +18,32 @@ library(fpc)
 
 # Load one person data
 #x1 = loadSinglePersonsData(300,4,0,"/home/nils/2017/group") #0=Alec (amazon server path)
-x1 = loadSinglePersonsData(300,4,0,"A:/Machine_Learning/2017/group") #0=Alec (local PC path)
+x1 = loadSinglePersonsData(300, 4, 0, "A:/Machine_Learning/2017/group") #0=Alec (local PC path)
 #array for the cipher instances
-cipher_array <- array(0,dim=c(50,dim(x1)[2]))
+cipher_array <- array(0, dim = c(50, dim(x1)[2]))
 
 ####### 2. Get five instances of each cipher out of training set #######
-for(cipher_iter in 1:10)
+for (cipher_iter in 1:10)
 {
-  cipher = cipher_iter-1
+  cipher = cipher_iter - 1
   
-  for(i in 1:5){
-    row = (cipher*5)+i
-    row_train_set = (cipher*400)+i
+  for (i in 1:5) {
+    row = (cipher * 5) + i
+    row_train_set = (cipher * 400) + i
     
-    cipher_array[row,] <-x1[row_train_set,]
+    cipher_array[row, ] <- x1[row_train_set, ]
     #cat("Row: ",row, " values= ", cipher_array[row,1:100],"\n")
     
   }
 }
 ####### 2. Calculate distances and draw dendogram #######
-dist_ciphers <- dist(cipher_array[,2:dim(x1)[2]],  method = "euclidean") # calculate distances
+dist_ciphers <-
+  dist(cipher_array[, 2:dim(x1)[2]],  method = "euclidean") # calculate distances
 hc_ciphers <- hclust(dist_ciphers)
-plot(hc_ciphers, hang = -1, cex = 0.6, labels=cipher_array[,1])
+plot(hc_ciphers,
+     hang = -1,
+     cex = 0.6,
+     labels = cipher_array[, 1])
 
 ################################################################
 #
@@ -50,48 +54,62 @@ plot(hc_ciphers, hang = -1, cex = 0.6, labels=cipher_array[,1])
 ################################################################
 
 ####### 1. Obtain data for second person #######
-x2 = loadSinglePersonsData(300,4,2,"A:/Machine_Learning/2017/group") #0=Louis (local PC path)
+x2 = loadSinglePersonsData(300, 4, 2, "A:/Machine_Learning/2017/group") #0=Louis (local PC path)
 x_2p <- rbind2(x1, x2)
 #order the array so the ciphers are in correct order from 0-9
-x_2p <- x_2p[order(x_2p[,1]),]
+x_2p <- x_2p[order(x_2p[, 1]), ]
 
 #Step for each new cipher
-step = 800
+step = 800 # 2* 400 numbers for each person
+
+
+####### 2. Get 5 cluster out of each cipher #######
 clusterSize = 5 #amount of clusters
 
-####### 2. Get 5 cluster out of each #######
-for(cipher_iter in 1:10)
+cipher_cluster <- c()
+label_cluster <- c()
+for (i in 1:10)
 {
-  cipher = cipher_iter-1
+  cipher = i - 1
   rowMin = cipher * step
-  rowMax =  rowMin + step    
+  rowMax =  rowMin + step
   
-  train_set <- x_2p[rowMin:rowMax,2:dim(x_2p[2])]
-  cipher_cluster <- c()
-  label_cluster <- c()
+  train_set <- x_2p[rowMin:rowMax, ]
   
-  #for each cipher, define clusters
-  for( i in 0:9) {
-    #training data for cipher i (remove class-identifier in first col)
-    train_data_i <- dataset_train[dataset_train[,1] == i, -1]
-    #calculate kmeans with clusterSize clusters
-    clusterData <- kmeans(train_data_i, clusterSize)
-    
-    #add center and label of the ith cipher to array
-    cipher_cluster[[i + 1]] <- clusterData$centers
-    label_cluster[[i + 1]] <- c(1:clusterSize)*0 + i
-    # uncomment the following line to plot the cluster
-    #plotcluster(train_data_i, clusterData$cluster)
-    
-    
-  }
+  #training data for cipher i (remove class-identifier in first col)
+  train_data_i <- train_set[train_set[, 1] == cipher,-1]
+  #calculate kmeans with clusterSize clusters
+  clusterData <- kmeans(train_data_i, clusterSize)
   
-  rm(train_data_i)
-  #get training-label and data for knn-algorithm out of clustered data
-  train_lab <- factor(unlist(label_cluster))
-  train_dat <- cipher_cluster[[1]]
-  for( i in 2:10) {
-    train_dat <- rbind(train_dat,cipher_cluster[[i]])
-  }
+  
+  #add center and label of the ith cipher to array
+  cipher_cluster[[cipher + 1]] <- clusterData
+  label_cluster[[cipher + 1]] <- c(1:clusterSize) * 0 + cipher
+  # uncomment the following line to plot the cluster
+  #plotcluster(train_data_i, clusterData$cluster)
+  
+  
 }
 
+rm(train_data_i)
+
+#get training-label and data for knn-algorithm out of clustered data
+train_lab <- factor(unlist(label_cluster))
+train_dat <- array(0,dim= c(0,3364))
+for (i in 1:10) {
+  for(c in 1:5){
+    train_dat <- rbind(train_dat, cipher_cluster[[i]]$centers[c])
+  }
+  
+}
+
+
+####### 3. Draw dendogram on the 50 clusters #######
+dist_ciphers <-
+  dist(train_dat,  method = "euclidean") # calculate distances
+hc_ciphers <- hclust(dist_ciphers)
+plot(hc_ciphers,
+     main="2 Person Data kmeans 5 instances/digit",
+     hang = -1,
+     cex = 0.6,
+     labels = train_lab)
---
+++ Exercise4/exercise_4_nils.R
@@ -0,0 +1,181 @@
+source("A:/Machine_Learning/Basefolder/loadImage.R")
+
+################################################################
+#
+# Function to load multiple persons data
+#
+################################################################
+
+loadMultiplePersonsData <-
+  function(dpi = 300,
+           startgrp = 4,
+           endgrp = 4,
+           location)
+  {
+    options(show.error.messages = FALSE)
+    memstart = 0
+    x = try(loadSinglePersonsData(dpi, startgrp, 0, location))
+    #DPI change
+    if (class(x) == "try-error")
+    {
+      x = loadSinglePersonsData(dpi, startgrp, 1, location)#DPI change
+      memstart = 1
+    }
+    
+    for (i in startgrp:endgrp)
+    {
+      for (j in 0:4)
+      {
+        if (j != memstart || i != startgrp)
+        {
+          y = try(loadSinglePersonsData(dpi, i, j, location))
+          #DPI change
+          if (class(y) != "try-error" && y[1, 1] == 0)
+          {
+            x <- rbind2(y, x)
+          }
+        }
+      }
+    }
+    options(show.error.messages = TRUE)
+    return(x)
+  }
+
+########################################################
+#
+# 0. Prerequisites:
+# Load data and apply pca
+#
+########################################################
+
+
+
+#select which you want. load only once. takes forever
+x = loadSinglePersonsData(300, 4, 3, "A:/Machine_Learning/2017/group")
+#left to right the inputs are: (dpi,start_group,end_group,file_location)
+x_l = loadMultiplePersonsData(300, 4, 4, "A:/Machine_Learning/2017/group")#loading group 3-8 takes ca. 30 min
+
+person_dependent = TRUE
+use_multi = TRUE
+test_split = 0.5  #how large should the training set be 0.9=90/10 training/testing
+
+set.seed(990)
+
+if (use_multi)
+{
+  if (person_dependent)
+  {
+    dataset_shuffle <- x_l[sample(nrow(x_l)), ]
+  } else
+  {
+    dataset_shuffle <- x_l
+  }
+} else
+{
+  dataset_shuffle <- x[sample(nrow(x)), ]
+  print("no use_multi ")
+}
+
+#create the training set
+dataset_train <-
+  array(, dim = c((dim(dataset_shuffle)[1] * test_split), dim(dataset_shuffle)[2]))
+for (i in 1:dim(dataset_train)[1])
+{
+  #training set
+  dataset_train[i, ] <- dataset_shuffle[i, ]
+}
+
+#create the testing set
+dataset_test <-
+  array(, dim = c(dim = c((
+    dim(dataset_shuffle)[1] - dim(dataset_train)[1]
+  ), dim(dataset_shuffle)[2])))
+for (i in 1:dim(dataset_test)[1])
+{
+  #kNN testing set
+  dataset_test[i, ] <-
+    dataset_shuffle[i + (dim(dataset_shuffle)[1] * test_split), ]
+}
+
+#training set classification vector
+train_class <- array(, dim = c(1, dim(dataset_train)[1]))
+for (i in 1:dim(dataset_train)[1])
+{
+  train_class[i] = dataset_train[i, 1]
+}
+
+#testing set classification vector
+test_class <- array(, dim = c(1, dim(dataset_test)[1]))
+for (i in 1:dim(dataset_test)[1])
+{
+  test_class[i] = dataset_test[i, 1]
+}
+
+print("Beginning pca on dataset")
+#pca <- prcomp(dataset_shuffle)
+rm(dataset_shuffle)
+
+
+pca_time <- proc.time()
+
+pca_train <- prcomp(dataset_train)
+
+pca_test <- prcomp(dataset_test)
+#pca_test<-dataset_test %*% pca_train$rotation
+
+pca_time <- proc.time() - pca_time
+#time is third variable
+
+#Plot and resume results
+
+#variance
+plot(pca_train$sdev,
+     main = "Variance by PCA",
+     xlab = "PCA",
+     ylab = "Variance")
+
+#accumulated variance absolute
+total_var = 0                                         #maximum variance counter variable
+total_var_plot = array(, dim = c(1, length(pca_train$sdev)))    #array tracking accumulated total variance(absolute)
+for (i in 1:length(pca_train$sdev))
+{
+  total_var <- total_var + pca_train$sdev[i]
+  total_var_plot[i] <- total_var
+}
+plot(
+  1:3365,
+  total_var_plot,
+  main = "Accumulated Variance (absolute)",
+  xlab = "PC",
+  ylab = "Acumulated Variance"
+)
+
+
+#accumulated variance percentage
+acc_var = 0                                           #accumulated variance
+acc_var_plot = array(, dim = c(1, length(pca_train$sdev)))      #array tracking accumulated variance(%)
+for (i in 1:length(pca_train$sdev))
+{
+  acc_var <- acc_var + pca_train$sdev[i]
+  acc_var_plot[i] <- acc_var / (total_var / 100)
+}
+plot(1:3365,
+     acc_var_plot,
+     main = "Accumulated Variance (%)",
+     xlab = "PC",
+     ylab = "Acumulated Variance")
+
+
+#20 values output in array
+acc_var_out <- array(0, dim = c(20))
+for (i in 1:20)
+{
+  acc_var_out[i] <- acc_var_plot[i * length(acc_var_out / 20)]
+}
+
+########################################################
+#
+# 4.1.1 Decision point and information gain
+# 5 graphs, one for each component, and the highest information gain
+#
+########################################################
