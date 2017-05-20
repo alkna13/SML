d0c00d982746325b63e81ba36416dec9a15ce14d
nilsjansen <nj_jansen@t-online.de>
2017-05-20 22:32:57 +0200

    added multiple runs for multiple numbers of first PCAs to knn

--- final_project/knn.R
+++ final_project/knn.R
@@ -6,12 +6,12 @@
 #######################################################
 
 #### 1. Source data script ####
-source("preProcessing_runs/loadImage.R")
+source("A:/Machine_Learning_Git/SML/final_project/loadImage.R")
 library(reshape2)
 library(ggplot2)
 
 #use preprocessed folder (DONT RENAME THE FOLDER) for centering(automatic corner and contour detection with rotation and centering automation)
-pathToGroups = "2017/group"
+pathToGroups = "A:/Machine_Learning/2017/group"
 #output folder to put all result documents in
 outputFolder = "pca1"
 
@@ -20,7 +20,7 @@ dpi = 300
 smoothing = "none" # can be "gaussian" or "average" or "none"/NULL
 sigma = 0 #only needed if smoothing = "gaussian"
 pre_pca = TRUE
-normalization = FALSE
+normalization = TRUE
 group = 4 #put in number for single group or "all" for whole course
 member = "all" #put in a nr for single person or "all" for whole group
 
@@ -225,23 +225,37 @@ if (pre_pca) {
   pca_train <- prcomp(dataset_train[, 2:dim(dataset_train)[2]])
   #apply the same manipullation on test set
   pca_test <- dataset_test[,2:dim(dataset_test)[2]] %*% pca_train$rotation
-  
-  if (normalization) {
-    dataset_train[,2:dim(dataset_train)[2]] <- normalize(pca_train$x)
-    dataset_test[,2:dim(dataset_test)[2]] <-  normalize(pca_train)
-  } else{
-    dataset_train[,2:dim(dataset_train)[2]] <- pca_train$x
-    dataset_train[,2:dim(dataset_train)[2]] <- pca_test
-  }
+  #save time for PCA
   end.time.knn <- proc.time()-start.time
   print(paste("Time for PCA: ",end.time.knn[3]))
+  
+  dataset_train <- array(,dim=c(dim(dataset_train)[1],(dim(pca_train$x)[2]+1)))
+  dataset_test <- array(,dim=c(dim(dataset_test)[1],(dim(pca_test)[2]+1)))
+  
+  if (normalization) {
+    start.time <- proc.time()
+    dataset_train[,2:dim(dataset_train)[2]] <- normalize(pca_train$x)
+    dataset_test[,2:dim(dataset_test)[2]] <-  normalize(pca_test)
+    #save time for norm
+    end.time.norm <- proc.time()-start.time
+    print(paste("Time for normalisation: ",end.time.norm[3]))
+    #add time in output array
+    data_out[, 9] = paste("TRUE (t=",end.time.norm[3],"s")
+  } else{
+    dataset_train [,2:dim(dataset_train)[2]] <- pca_train$x
+    dataset_test [,2:dim(dataset_test)[2]]<- pca_test
+  }
+  
+  #add class identifiers again
+  dataset_train[,1] <- train_class
+  dataset_test[,1] <- test_class
+  #name the columns
+  colnames(dataset_train)<-c("cipher",paste("PCA", 1:(dim(dataset_train)[2]-1)))
+  colnames(dataset_test)<-c("cipher",paste("PCA", 1:(dim(dataset_test)[2]-1)))
   #add time in output array
   data_out[, 8] = paste("TRUE (t=",end.time.knn[3],"s")
   
   
-  #add class identifiers again
-  dataset_train[,1] <- train_class[i]
-  dataset_test[,1] <- test_class[i]
   
   #Evaluate PCA
   subText = paste("Group: ",group,"member: ", member,"| Split:", splitText)
@@ -286,100 +300,106 @@ if (pre_pca) {
   dev.copy(png, filename = paste(outputFolder, "/acc_variance_perc.png", sep =""))
   dev.off ()
   write.csv2(total_var_plot, file = paste(outputFolder,"/acc_variance_perc.csv",sep="")) # save file for all results
-} 
+} # end of pca
 
 #remove unnecessary variables
 rm(x)
 
 
-
 #### 6. Begin knn ####
-total_knn_start_time <- proc.time()
-
-k = k_start
-k_run = 0 #iterator for k-runs
-while (k <= k_end)
-{
-  print (paste("knn for k=", k, " is being calculated ..."))
-  k_run <- k_run + 1
-  #start timing
-  ptm <- proc.time()
+#vector with different # of PCAs to use in each run
+firstXPCAs<- c(50,100,200,400,600,1000,2000)
+for(f in 1:length(firstXPCAs)){
   
-  #run knn test
-  test_pred <-
-    knn(dataset_train[, 2:dim(dataset_train)[2]], dataset_test[,2:dim(dataset_test)[2]], train_class, k)
   
-  #stop timing
-  ptm <- proc.time() - ptm
   
-  #check accuracy
-  correct = 0
-  incorrect = 0
-  for (i in 1:dim(dataset_test)[1])
+  total_knn_start_time <- proc.time()
+  
+  k = k_start
+  k_run = 0 #iterator for k-runs
+  while (k <= k_end)
   {
-    if (test_pred[i] == test_class[i])
+    print (paste("knn for k=", k, " is being calculated ..."))
+    k_run <- k_run + 1
+    #start timing
+    ptm <- proc.time()
+    
+    #run knn test
+    test_pred <-
+      # RESET LATER: knn(dataset_train[, 2:dim(dataset_train)[2]], dataset_test[,2:dim(dataset_test)[2]], train_class, k)
+      knn(dataset_train[, 2:(firstXPCAs[f]+1)], dataset_test[,2:(firstXPCAs[f]+1)], train_class, k)
+    #stop timing
+    ptm <- proc.time() - ptm
+    
+    #check accuracy
+    correct = 0
+    incorrect = 0
+    for (i in 1:dim(dataset_test)[1])
     {
-      correct = correct + 1
+      if (test_pred[i] == test_class[i])
+      {
+        correct = correct + 1
+      }
+      else
+      {
+        incorrect = incorrect + 1
+      }
     }
-    else
-    {
-      incorrect = incorrect + 1
-    }
+    accuracy = correct / dim(dataset_test)[1] * 100
+    
+    
+    #fill last 3 columns of output array
+    row = (k_run)
+    data_out[row, 11] = k
+    data_out[row, 12] = accuracy
+    data_out[row, 13] = ptm[3]
+    
+    #increase k
+    k = k + k_inc
   }
-  accuracy = correct / dim(dataset_test)[1] * 100
   
   
-  #fill last 3 columns of output array
-  row = (k_run)
-  data_out[row, 11] = k
-  data_out[row, 12] = accuracy
-  data_out[row, 13] = ptm[3]
+  total_time <- proc.time() - total_knn_start_time
+  cat("Total process time: ", total_time[3] / 60, " minutes")
   
-  #increase k
-  k = k + k_inc
+  #calculate total values
+  accuracies <- as.numeric(data_out[, 12])
+  times <- as.numeric(data_out[, 13])
+  total_values <- array(0, c(1, 5))
+  accAvg = mean(accuracies)
+  varAcc = (sum((accuracies - accAvg) ^ 2)) / length(accuracies)
+  stdev = sqrt(varAcc)
+  timeAvg = mean(times)
+  timeTotal = sum(times)
+  
+  colnames(total_values) <-
+    c("Accuracy_Avg", "Variance", "StdDev", "Time_Avg", "Time_Total")
+  total_values[1, 1] <- accAvg
+  total_values[1, 2] <- varAcc
+  total_values[1, 3] <- stdev
+  total_values[1, 4] <- timeAvg
+  total_values[1, 5] <- timeTotal
+  
+  #plot graph with accuracy
+  #preparation
+  results <- as.data.frame(data_out)
+  results <- results[order(results$k), ] #order
+  k <- results$k
+  accuracy <- results$accuracy
+  performance <- results$`k_time [s]`
+  df <- data.frame(k, accuracy, performance)
+  df.melted <- melt(df, id = "k")
+  
+  ggplot(data = df.melted, aes(x = reorder(k), y = value, color = variable)) +
+    geom_point() +
+    labs(title = "Accuracy and Performance for knn", x = "k", y = "Value\n(Performances: Time in sec | Accuracy: Percentage %)") +
+    facet_grid(variable ~ ., scales = 'free')
+  
+  # Save table and plots
+  ggsave(filename = paste(outputFolder, "/plot_knn_PCAs",firstXPCAs[f],".png", sep = ""))
+  textFilename1 = paste(outputFolder, "/all_results_PCAs",firstXPCAs[f],".csv", sep = "")
+  textFilename2 = paste(outputFolder, "/total_results_PCAs",firstXPCAs[f],".csv", sep = "")
+  
+  write.csv2(data_out, file = textFilename1) # save file for results of each k
+  write.csv2(total_values, file = textFilename2) # save file for total sum results
 }
-
-
-total_time <- proc.time() - total_knn_start_time
-cat("Total process time: ", total_time[3] / 60, " minutes")
-
-#calculate total values
-accuracies <- as.numeric(data_out[, 12])
-times <- as.numeric(data_out[, 13])
-total_values <- array(0, c(1, 5))
-accAvg = mean(accuracies)
-varAcc = (sum((accuracies - accAvg) ^ 2)) / length(accuracies)
-stdev = sqrt(varAcc)
-timeAvg = mean(times)
-timeTotal = sum(times)
-
-colnames(total_values) <-
-  c("Accuracy_Avg", "Variance", "StdDev", "Time_Avg", "Time_Total")
-total_values[1, 1] <- accAvg
-total_values[1, 2] <- varAcc
-total_values[1, 3] <- stdev
-total_values[1, 4] <- timeAvg
-total_values[1, 5] <- timeTotal
-
-#plot graph with accuracy
-#preparation
-results <- as.data.frame(data_out)
-results <- results[order(results$k), ] #order
-k <- results$k
-accuracy <- results$accuracy
-performance <- results$`k_time [s]`
-df <- data.frame(k, accuracy, performance)
-df.melted <- melt(df, id = "k")
-
-ggplot(data = df.melted, aes(x = reorder(k), y = value, color = variable)) +
-  geom_point() +
-  labs(title = "Accuracy and Performance for knn", x = "k", y = "Value\n(Performances: Time in sec | Accuracy: Percentage %)") +
-  facet_grid(variable ~ ., scales = 'free')
-
-# Save table and plots
-ggsave(filename = paste(outputFolder, "/plot_knn.png", sep = ""))
-textFilename1 = paste(outputFolder, "/all_results.csv", sep = "")
-textFilename2 = paste(outputFolder, "/total_results.csv", sep = "")
-
-write.csv2(data_out, file = textFilename1) # save file for all results
-write.csv2(total_values, file = textFilename2) # save file for total sum results
